bf73e5e35118da9d61016d130438e444d9ff7428
josephjvalavi <77263983+josephjvalavi@users.noreply.github.com>
2021-09-08 22:41:36 +0530

    arithamatic test main

---
+++ Arithematictest.java
@@ -0,0 +1,14 @@
+package arithmetic;
+public class Test
+{
+	public static void main(String args[])
+	{
+		TestPackage obj=new TestPackage();
+		System.out.println("Addition: "+obj.Add(10,10));
+		System.out.println("Subtraction: "+obj.Sub(10,10));
+		System.out.println("Multiplication: "+obj.Multi(10,10));
+		System.out.println("Division: "+obj.Div(10,10));
+	}
+}
+		
+		
